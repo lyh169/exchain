bc128dd6a13d7971b779d4f5e49f26ceb8835d22
zhangkai <zhangkai.gis@163.com>
2022-04-02 20:55:45 +0800

    use old logical to test init cmd

--- libs/ibc-go/testing/simapp/simd/cmd/cmd_test.go
+++ libs/ibc-go/testing/simapp/simd/cmd/cmd_test.go
@@ -1,24 +1,61 @@
 package cmd_test
 
 import (
-	"fmt"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-
-	svrcmd "github.com/okex/exchain/libs/cosmos-sdk/server"
+	"github.com/okex/exchain/libs/cosmos-sdk/codec"
+	"github.com/okex/exchain/libs/cosmos-sdk/server"
+	"github.com/okex/exchain/libs/cosmos-sdk/tests"
+	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/libs/cosmos-sdk/x/genutil/client/cli"
-	"github.com/okex/exchain/libs/ibc-go/testing/simapp"
-	"github.com/okex/exchain/libs/ibc-go/testing/simapp/simd/cmd"
+	tcmd "github.com/okex/exchain/libs/tendermint/cmd/tendermint/commands"
+	"github.com/okex/exchain/libs/tendermint/libs/log"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
-func TestInitCmd(t *testing.T) {
-	rootCmd, _ := cmd.NewRootCmd()
-	rootCmd.SetArgs([]string{
-		"init",        // Test the init cmd
-		"simapp-test", // Moniker
-		fmt.Sprintf("--%s=%s", cli.FlagOverwrite, "true"), // Overwrite genesis.json, in case it already exists
-	})
+const (
+	flagOverwrite  = "overwrite"
+	flagClientHome = "home-client"
+)
 
-	require.NoError(t, svrcmd.Execute(rootCmd, simapp.DefaultNodeHome))
+//func TestInitCmd(t *testing.T) {
+//	rootCmd, _ := cmd.NewRootCmd()
+//	rootCmd.SetArgs([]string{
+//		"init",        // Test the init cmd
+//		"simapp-test", // Moniker
+//		fmt.Sprintf("--%s=%s", cli.FlagOverwrite, "true"), // Overwrite genesis.json, in case it already exists
+//	})
+//
+//	require.NoError(t, svrcmd.Execute(rootCmd, simapp.DefaultNodeHome))
+//}
+func TestInitCmd(t *testing.T) {
+	defer server.SetupViper(t)()
+	defer setupClientHome(t)()
+	home, cleanup := tests.NewTestCaseDir(t)
+	defer cleanup()
+
+	logger := log.NewNopLogger()
+	cfg, err := tcmd.ParseConfig()
+	require.Nil(t, err)
+
+	ctx := server.NewContext(cfg, logger)
+	cdc := makeCodec()
+	cmd := cli.InitCmd(ctx, cdc, testMbm, home)
+
+	require.NoError(t, cmd.RunE(nil, []string{"appnode-test"}))
+}
+
+func setupClientHome(t *testing.T) func() {
+	clientDir, cleanup := tests.NewTestCaseDir(t)
+	viper.Set(flagClientHome, clientDir)
+	viper.Set(flagOverwrite, true)
+	return cleanup
+}
+
+// custom tx codec
+func makeCodec() *codec.Codec {
+	var cdc = codec.New()
+	sdk.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+	return cdc
 }
