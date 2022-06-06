7d5a4a894a641f18e696699b022c7c30cd37a448
zhangkai <zhangkai.gis@163.com>
2022-06-06 20:35:23 +0800

    add custom list

---
+++ x/evm/watcher/message_list.go
@@ -0,0 +1,37 @@
+package watcher
+
+import (
+	"container/list"
+	stdlog "log"
+	"sync"
+)
+
+type idleWatchMessagesList struct {
+	mutex    sync.Mutex
+	idleList *list.List
+}
+
+func newIdleWatchMessageList() *idleWatchMessagesList {
+	return &idleWatchMessagesList{
+		idleList: list.New(),
+	}
+}
+
+func (l *idleWatchMessagesList) getWatchMessages() []WatchMessage {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if l.idleList.Len() == 0 {
+		stdlog.Println("giskook new")
+		return make([]WatchMessage, 0)
+	}
+	front := l.idleList.Remove(l.idleList.Front())
+
+	return front.([]WatchMessage)
+}
+
+func (l *idleWatchMessagesList) putWatchMessages(msg []WatchMessage) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	l.idleList.PushBack(msg)
+}
